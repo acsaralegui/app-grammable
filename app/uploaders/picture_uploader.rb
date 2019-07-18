efbcb6ce17f5171560009b173ca6773e0c5e4ea6
acsaralegui <aaron.saralegui@gmail.com>
2019-07-18 16:44:44 +0000

    add validation for images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,11 +3,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-   storage :aws
-  else
-    storage :file
-  end
+  
+   storage :fog
+  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
