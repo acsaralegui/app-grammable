cb796997edafadc0e2e55da54f77d5b393030fb5
acsaralegui <aaron.saralegui@gmail.com>
2019-07-21 21:40:18 +0000

    fix model

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,11 +1,7 @@
-class User < ApplicationRecord
+
+class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :grams
-  has_many :comments
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Grammable::Application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users
-  root "grams#index"
-  resources :grams do
-    resources :comments, only: :create
-  end
+  devise_for :models
+  root "grams#index" # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :grams do 
+  resources :comments, only: :create
+ end
 end
