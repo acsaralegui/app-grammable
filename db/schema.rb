8764bb4b91fa0a98e3def6acab97aabd2dbeb02b
acsaralegui <aaron.saralegui@gmail.com>
2019-07-22 04:22:45 +0000

    trying to fix heroku migration

--- db/schema.rb
+++ db/schema.rb
@@ -27,23 +27,11 @@ ActiveRecord::Schema.define(version: 2019_07_18_044451) do
 
   create_table "grams", force: :cascade do |t|
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_grams_on_user_id"
-  end
-
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
