1901216f3fbbab2e04940cf44b7f16eaaaed0418
acsaralegui <aaron.saralegui@gmail.com>
2019-07-22 03:58:18 +0000

    fixing error relation comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  describe "comments#create action" do
+  describe 'grams#create action'
     it "should allow users to create comments on grams" do
       gram = FactoryBot.create(:gram)
 
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+      post :create, params: { gram_id: gram.id, comment: { message: "awesome gram" } }
+
       expect(response).to redirect_to root_path
       expect(gram.comments.length).to eq 1
       expect(gram.comments.first.message).to eq "awesome gram"
-    end
+    end 
 
-    it "should require a user to be logged in to comment on a gram" do
+    it "should require a user to be logged in to comment on a gram" do 
       gram = FactoryBot.create(:gram)
-      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+      post :create, params: { gram_id: gram.id, comment: { message: "awesome gram" } }
       expect(response).to redirect_to new_user_session_path
+    end 
 
-    end
-
-    it "should return http status code of not found if the gram isn't found" do
+    it "should return http status code of not found if the gram isn't found" do 
       user = FactoryBot.create(:user)
       sign_in user
-      post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' } }
+
+      post :create, params: { gram_id: 'YOLOSWAG', comment: { message: "awesome gram" } }
       expect(response).to have_http_status :not_found
-    end
-  end
-end
+    end 
+  end 
